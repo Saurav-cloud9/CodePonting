b26d2197b897419506772a32001f4aea41e7fd0c
Saurav-cloud9 <sauravzmail@gmail.com>
2026-05-18 20:14:56 +0530

    desktop upate

--- Learning/Codedex/python intermediate/classes_revisit/exercise35_bob's_burger.py
+++ Learning/Codedex/python intermediate/classes_revisit/exercise35_bob's_burger.py
@@ -11,4 +11,13 @@ bobs_burgers.category = "American Diner"
 bobs_burgers.rating = 4.7
 bobs_burgers.delivery = False
 
+ghar_ka_dhaba = Restaurants()
+
+ghar_ka_dhaba.name = "Ghar Ka Dhaba"
+ghar_ka_dhaba.category = "Desi Restaurant"
+ghar_ka_dhaba.rating = 4.8
+ghar_ka_dhaba.delivery = True
+
 print(vars(bobs_burgers))
+print(vars(ghar_ka_dhaba))
+
